899aef29a65d92870690e920d430c242bc5c4418
Armaanjasim <58048776+Armaanjasim@users.noreply.github.com>
2022-01-24 13:43:54 +0000

    Data Types file added

---
+++ 01_Variables/03_Data_Types.py
@@ -0,0 +1,86 @@
+#  Numerical
+
+# i = 375  # int
+# f = 3.75  # float
+# c = 3j  # complex
+
+
+# add = 3 + 5
+# subtract = 3 - 5
+# multiply = 3 * 5
+# divide = 3 / 5  # // rounds down to the lowest number
+# power = 3 ** 5
+# modulo = 3 % 5
+
+
+#  Strings
+
+# single = 'string in a single quotes'
+# double = "string in double quotes"
+
+# failure = 'this is david's string'
+# fixed = "this is david's string"
+
+# Indexing and Slicing
+
+# hi = "Hello World!"
+# print(hi[0])  # Python starts counting at zero
+# print(hi[-1])  # Starting from the end of the string
+# print(hi[3:8])  # Prints Lo Wo
+# print(len(hi))  # Length of the string
+
+
+# Methods
+
+# white_space = "    lots of white space    "
+# print(len(white_space))
+# print(white_space.strip())
+# print(white_space.strip().upper())
+# print(white_space.strip().capitalize())
+# print(white_space.count(" "))
+# print(white_space.strip().replace("o", "0"))
+
+
+# F-Strings (formatted strings)
+
+# pi = 3.14159265359
+# print(f"pi to 3dp: {pi:.3f}")
+# print(f"pi to 5dp: {pi:.5f}")
+# print(f"pi to 6dp: {pi:.6f}")
+#
+# score = 16
+# max_score = 50
+#
+# print(f"You scored: {score / max_score}")
+# print(f"You scored: {score / max_score:.0%}")
+
+
+# Boolean
+
+# t = True
+# f = False
+#
+# print(t, type(t))
+#
+# print(3 + 2 == 5)
+# print(5 != 5)
+
+
+#
+
+# hi = "hello world"
+# print(hi.isalpha())
+# print(hi.islower())
+# print(hi.upper().isupper())
+# print(hi.endswith("rld"))
+# print(hi.startswith("hello"))
+
+#print(bool(""))
+
+
+
+
+
+
+
+
