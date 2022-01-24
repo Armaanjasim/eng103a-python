b137cf2a8693a4054d2628d848c798fa19833810
Armaanjasim <58048776+Armaanjasim@users.noreply.github.com>
2022-01-24 11:39:11 +0000

    Input Intro files added

---
+++ 01_Variables/02_Input_Intro.py
@@ -0,0 +1,9 @@
+name = input("What is your name?\n")
+
+print("Hello, " + name)
+
+number1 = int(input("\nPick a number between 1-10: "))
+number2 = int(input("Pick a number you wish to multiple the previous number by: "))
+number3 = int(input("Pick a number you wish to divide the sum of the previous number by: "))
+
+print("Result:", (number1 * number2)/number3)
