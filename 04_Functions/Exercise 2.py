fa3c4c1fa0f50febd13c795e7f02c5ea9b43ca37
Armaanjasim <58048776+Armaanjasim@users.noreply.github.com>
2022-01-25 23:35:52 +0000

    finished exercise 2 still some bugs for last question

--- 04_Functions/Exercise 2.py
+++ 04_Functions/Exercise 2.py
@@ -20,7 +20,7 @@ def integer_divisor(num1=0):
     return new_list
 
 
-print(integer_divisor())
+# print(integer_divisor())
 
 print("\nQ1b\n")
 # Q1b: Write a function which takes in two integers as arguments and returns true if one of the numbers
@@ -60,7 +60,7 @@ def factor_of_two_numbers():
             print(False)
 
 
-factor_of_two_numbers()
+# factor_of_two_numbers()
 
 
 # -------------------------------------------------------------------------------------- #
@@ -70,8 +70,32 @@ print("\nQ2a\n")
 # alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 #             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
-# A2a:
 
+# A2a:
+def string_into_alphabet(string_letter=False):
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    index = 0
+    skip_this_part = True
+    if not string_letter:
+        string_letter = input("Please type a letter to know what place it is in the alphabet: ")
+    else:
+        skip_this_part = False
+    if string_letter.isalpha():
+        if len(string_letter) == 1:
+            index = alphabet.index(string_letter.lower()) + 1
+            if skip_this_part:
+                print(index)
+        else:
+            print("Please enter 1 character only!")
+            string_into_alphabet()
+    else:
+        print("Please enter an alphabet character only!")
+        string_into_alphabet()
+    return index
+
+
+# string_into_alphabet()
 
 
 print("\nQ2b\n")
@@ -79,16 +103,43 @@ print("\nQ2b\n")
 # ID number consisting of the positions of each letter in the name
 # e.g. f("bob") = "1141" as "b" is in position 1 and "o" is in position 14
 
+
 # A2b:
+def persons_name_into_id():
+    alphabet = ["123", "456", "789", "101", "112", "131", "415", "161", "718", "192", "021", "222", "324",
+                "252", "627", "282", "930", "313", "233", "343", "536", "373", "839", "404", "142", "434"]
+    string_name = input("Please enter your name to convert into an id: ")
+    code_name = ""
+    if string_name.isalpha():
+        for char in string_name:
+            x = string_into_alphabet(char)
+            # print(alphabet[x])
+            code_name += alphabet[x-1]
+    else:
+        print("Silly your name can't have a number or space. Enter an alphabet character only!")
+        persons_name_into_id()
+    print(f"Your code name is: {code_name}")
+    return code_name
 
 
+# persons_name_into_id()
 
 print("\nQ2c\n")
 # Q2c: Create a function which turns this ID into a password. The function should subtract
 # the sum of the numbers in the id that was generated from the whole number of the id.
 # e.g. f("bob") -> 1134 (because bob's id was 1141 and 1+1+4+1 = 7 so 1141 - 7 = 1134)
 
+
 # A2c:
+def turn_id_into_password():
+    name_id = persons_name_into_id()
+    for number in name_id:
+        name_id = int(name_id) - int(number)
+    print(f"Password is: {name_id}")
+
+
+# turn_id_into_password()
+
 
 
 
@@ -97,15 +148,34 @@ print("\nQ2c\n")
 print("\nQ3a\n")
 # Q3a: Write a function which takes an integer as an input, and returns true if the number is prime, false otherwise.
 
+
 # A3a:
-
-
+def check_prime_number():
+    number = input_number("What number would you like to check is a prime number? ")
+    ans = "Prime"
+    if number:
+        for i in range(2, int(number)):
+            if int(number) % i == 0:
+                ans = "Not Prime"
+                break
+    else:
+        print("You need a digit please!")
+        check_prime_number()
+    print(ans)
 
 print("\nQ3b\n")
 # Q3b: Now add some functionality to the function which does not error if the user inputs something other than a digit
 
+
 # A3b:
+def input_number(question):
+    ans = input(question)
+    if ans.isdigit():
+        return ans
+    else:
+        return False
 
 
+check_prime_number()
 
 # -------------------------------------------------------------------------------------- #
