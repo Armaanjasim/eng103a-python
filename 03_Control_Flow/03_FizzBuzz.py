4ed54c89b7666be4f1a53d9a1273975abe443d9b
Armaanjasim <58048776+Armaanjasim@users.noreply.github.com>
2022-01-25 10:11:02 +0000

    Edited fizz buzz and added while loops file

--- 03_Control_Flow/03_FizzBuzz.py
+++ 03_Control_Flow/03_FizzBuzz.py
@@ -2,7 +2,7 @@
 
 while True:
     startNum = input("Pick a starting number for fizzbuzz: ")
-    if startNum.isnumeric() and int(startNum) > 0 and int(startNum) > 0:
+    if startNum.isdigit() and int(startNum) > 0 and int(startNum) > 0:
         # Check if the input is a number and greater than 0
         break
         # If the input meets the requirements it breaks from the while loop
@@ -12,7 +12,7 @@ while True:
 
 while True:
     endNum = input("Pick an ending number for fizzbuzz: ")
-    if endNum.isnumeric() and int(endNum) > 0:
+    if endNum.isdigit() and int(endNum) > 0:
         # Check if the input is a number and greater than 0
         break
         # If the input meets the requirements it breaks from the while loop
@@ -20,8 +20,8 @@ while True:
         print("Pick an integer please")
         # If the input doesn't meet the requirements it prints the statement and then loops till it does
 
-word1 = input("Input first keyword for numbers divisible by 3: ")
-word2 = input("Input second keyword for numbers divisible by 5: ")
+word1 = input("Input first keyword for numbers divisible by 3: ")  # Could use isalpha() to check input
+word2 = input("Input second keyword for numbers divisible by 5: ")  # Could use isalpha() to check input
 
 for i in range(int(startNum), int(endNum)+1):
     if i % 3 == 0 and i % 5 == 0:  # Checks if the number is divisible by 3 and 5
