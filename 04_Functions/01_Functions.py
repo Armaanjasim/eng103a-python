d358e0cf33dedc779b161605cade11c52b881e09
Armaanjasim <58048776+Armaanjasim@users.noreply.github.com>
2022-01-25 11:28:20 +0000

    functions started

---
+++ 04_Functions/01_Functions.py
@@ -0,0 +1,33 @@
+# def woof(number_of_woofs):
+#     for i in range(number_of_woofs):
+#         print("Woof!")
+#
+#
+# def greeting(name):
+#     print(f"Hello to you, {name}")
+#
+#
+# greeting("David")
+
+
+# def double_plus_num(num1, num2):
+#     ans = (num1 * 2) + num2
+#     return ans
+#
+# print(double_plus_num(5, 2))
+
+
+# def shopping(name, test, *items):  # multiargs
+#     for item in items:
+#         print(f"{name}! Don't forget to buy {item}s")
+#
+#
+# shopping("David", "Banana", "Apple", "Orange")
+
+
+# Type Hints
+# def greeting(name: str):
+#     print(f"Hello to you, {name}")
+#
+#
+# greeting(24601)
