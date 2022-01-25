a37615bcbf85cc322a6837cbad0e40b42eabed8b
Armaanjasim <58048776+Armaanjasim@users.noreply.github.com>
2022-01-25 10:42:48 +0000

    Added fizz buzz varibles instead of 3 and 5

--- 03_Control_Flow/03_FizzBuzz.py
+++ 03_Control_Flow/03_FizzBuzz.py
@@ -1,5 +1,6 @@
 # Basic Version
 
+#  While True Loop For Starting Number
 while True:
     startNum = input("Pick a starting number for fizzbuzz 1-10000: ")
     if startNum.isdigit() and 0 < int(startNum) < 10000:
@@ -10,6 +11,7 @@ while True:
         print("Pick an 'Integer' between '1-10000'")
         # If the input doesn't meet the requirements it prints the statement and then loops till it does
 
+#  While True Loop For Ending Number
 while True:
     endNum = input("Pick an ending number for fizzbuzz 1-10000: ")
     if endNum.isdigit() and 0 < int(endNum) < 10000:
@@ -20,6 +22,7 @@ while True:
         print("Pick an 'Integer' between '1-10000'")
         # If the input doesn't meet the requirements it prints the statement and then loops till it does
 
+#  While True Loop For First Word
 while True:
     word1 = input("Input first keyword for numbers divisible by 3 (Only alphabetical characters & No Spaces): ")
     if word1.isalpha():
@@ -30,6 +33,7 @@ while True:
         print("You must only select alphabetical characters 'a', 'A', 'b', 'B' etc")
         # If the input doesn't meet the requirements it prints the statement and then loops till it does
 
+#  While True Loop For Second Word
 while True:
     word2 = input("Input first keyword for numbers divisible by 5 (Only alphabetical characters & No Spaces): ")
     if word2.isalpha():
@@ -40,17 +44,22 @@ while True:
         print("You must only select alphabetical characters 'a', 'A', 'b', 'B' etc")
         # If the input doesn't meet the requirements it prints the statement and then loops till it does
 
+
 counter1 = 0
 counter2 = 0
 counter3 = 0
+
+fizz = 3
+buzz = 5
+
 for i in range(int(startNum), int(endNum)+1):
-    if i % 3 == 0 and i % 5 == 0:  # Checks if the number is divisible by 3 and 5
+    if i % fizz == 0 and i % buzz == 0:  # Checks if the number is divisible by 3 and 5
         print(word1 + word2)
         counter3 += 1  # Counter for how many times it has run through this if statemen
-    elif i % 3 == 0:  # Checks if the number is divisible by 3
+    elif i % fizz == 0:  # Checks if the number is divisible by 3
         print(word1)
         counter1 += 1  # Counter for how many times it has run through this if statemen
-    elif i % 5 == 0:  # Checks if the number is divisible by 5
+    elif i % buzz == 0:  # Checks if the number is divisible by 5
         print(word2)
         counter2 += 1  # Counter for how many times it has run through this if statement
     else:
